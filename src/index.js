62bb70251fe3c30fa4d6b993c44868d67fb227b4
RigelTLT <kozyrin-di@yandex.ru>
2022-01-25 19:22:13 +0400

    feat: add function decode

--- src/index.js
+++ src/index.js
@@ -39,6 +39,24 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    const searchTen = '10';
+    const replaceWithTen = '.';
+    const searchEleven = '11';
+    const replaceWithEleven = '-'; 
+    let arr = expr.match(/.{1,10}/g);
+    arr.forEach(function(arg, i){
+        if(arg !== '**********'){
+            arr[i] = String(parseInt(arr[i], 10));
+            arr[i] = arr[i].split(searchTen).join(replaceWithTen);
+            arr[i] = arr[i].split(searchEleven).join(replaceWithEleven);
+            arr[i] = MORSE_TABLE[`${arr[i]}`];
+        }
+        else{
+            arr[i] = ' ';
+        }
+    }
+    )
+    return arr.join('');
 }
 
 module.exports = {
